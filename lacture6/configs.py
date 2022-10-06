5a5fbf8b1881db12671c12a65ec427339edb9b0b
Minwatcharakorn <watcharakornminna@gmail.com>
2022-10-06 13:55:51 +0700

    move

--- lacture6/configs.py
+++
@@ -1,9 +0,0 @@
-### config.py ###
-
-#Scheme   "postgres+psycopg2: //<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-
-#DATEBASE_URI = sqlite:///books.sqlite3#
-DATABASE_URI = ''
-#DATE_URI = ''
-
-f_yam = ''
