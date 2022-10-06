4ed25ba642af1f10b8295e31852c513f8a5eb840
Minwatcharakorn <watcharakornminna@gmail.com>
2022-10-06 13:58:13 +0700

    move

--- EP2/models.py
+++ EP2/models.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 
 class User(Base):
-    ___tablename___ = 'user'
+    __tablename__ = 'user'
     id = Column(Integer,primary_key = True)
     name  = Column(String)
     fullname = Column(String)
