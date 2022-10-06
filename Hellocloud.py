89fc204ac316e7491f64be016015d4455293615e
Minwatcharakorn <watcharakornminna@gmail.com>
2022-10-06 13:47:51 +0700

    move

--- Hellocloud.py
+++ Hellocloud.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask , render_template
 
-app = Flask(__name__)
+app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello RUK-COM is Great</h1>'
+    return  '<h1> Hello RUK_COM is Great!</h1>'
 
-@app.route('/home', methods=['GET','POST'])
+@app.route('/home', methods = ['GET','POST'])
+
 def home():
-    links = ['https://ruk-com.in.th',
-             'https://www.google.com',
-             'https://www.python.org',
-             'https://www.kmutnb.ac.th']
-    return render_template('example.html', links=links)
+    links = [ 'https://ruk-com.in.th', 
+              'https://www.google.com ',
+              'https://www.python.org ', 
+              'https://www.kmutnb.ac.th' ]
+    return render_template ('example.html' , links = links) 
 
-if __name__=='__main__':
+print('Help')
+if __name__ == '__main__' :
     app.debug = True
-    app.run(host='0.0.0.0',port=80)
+    app.run (host='0.0.0.0', port=80)
